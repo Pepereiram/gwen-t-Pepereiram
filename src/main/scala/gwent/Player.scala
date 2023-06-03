039e86b582e794e7cfacdcd7ec4961951cbd3056
Pepereiram <89109716+Pepereiram@users.noreply.github.com>
2023-06-02 21:37:10 -0400

    REFACTOR
    Some methos have been removed from IPlayer and Player. The getters and setters have also been added. Changes were made accordingly.

--- src/main/scala/gwent/Player.scala
+++ src/main/scala/gwent/Player.scala
@@ -6,14 +6,27 @@ import card.ICard
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
 
-/** Class that represents a player in Gwent */
-class Player(val name: String, var gems: Int = 2,
-             val deck: ListBuffer[ICard], val hand: ListBuffer[ICard] ) extends IPlayer with Equals {
+/** Class that represents a player in Gwent
+ *
+ * The player could be user controlled or CPU controlled.
+ *
+ * @constructor Create a new Player in the game.
+ * @param _name the Player's name.
+ * @param _gems the Player's current gems.
+ * @param _deck the Player's current deck.
+ * @param _hand the Player's current hand.
+ *
+ * @author Jose Pereira Morgado
+ * @version 1.2
+ * @since 1.0
+ */
+class Player(val _name: String, var _gems: Int = 2,
+             val _deck: ListBuffer[ICard], val _hand: ListBuffer[ICard] ) extends IPlayer with Equals {
 
   override def equals(that: Any): Boolean = {
     if(canEqual(that)){
       val other = that.asInstanceOf[Player]
-      (this eq other) || (this.name == other.name && this.deck == other.deck && this.hand == other.hand)
+      (this eq other) || (this._name == other._name && this._deck == other._deck && this._hand == other._hand)
     } else {
       false
     }
@@ -21,54 +34,44 @@ class Player(val name: String, var gems: Int = 2,
   override def canEqual(that: Any): Boolean = {
     that.isInstanceOf[Player]
   }
-  override def drawCards(): Boolean = {
-    /* if there are cards in deck -> add to hand and true, if not false */
-    if (this.deck.isEmpty){
-      false
-    } else{
-      val card: ICard = this.deck.head
-      this.hand += card
-      this.deck.remove(0)
-      true
+  override def name: String = _name
+  override def gems: Int = _gems
+  override def gems_(value: Int): Unit = {
+    if (this._gems > 0) {
+      this._gems -= value
     }
   }
-
-  override def getName: String = this.name
-
-  override def getGems: Int = this.gems
-
-  override def setGems(value: Int): Boolean = {
-    if (this.gems > 0) {
-      this.gems -= value
-      true
-    } else {
-      false
-    }
-  }
-
-  override def getDeck: ListBuffer[ICard] = this.deck
-
-  override def getHand: ListBuffer[ICard] = this.hand
-
+  override def deck: ListBuffer[ICard] = _deck
+  override def hand: ListBuffer[ICard] = _hand
   override def shuffleDeck: Boolean = {
-    if (this.deck.nonEmpty) {
-      Random.shuffle(this.deck)
+    if (this._deck.nonEmpty) {
+      Random.shuffle(this._deck)
       true
     } else {
       println("Deck is empty")
       false
     }
   }
-
   override def playCard(card: ICard): Boolean = {
     /* if the card is in hand -> true and play it, if not false */
-    if (this.hand.contains(card) && this.hand.nonEmpty){
+    if (this._hand.contains(card) && this._hand.nonEmpty){
       //Not yet implemented
-      this.hand -= card
+      this._hand -= card
       true
     } else{
       println("Card not in hand")
       false
+    }
+  }
+  override def drawCards(): Boolean = {
+    /* if there are cards in deck -> add to hand and true, if not false */
+    if (this._deck.isEmpty) {
+      false
+    } else {
+      val card: ICard = this._deck.head
+      this._hand += card
+      this._deck -= card
+      true
     }
   }
 }
